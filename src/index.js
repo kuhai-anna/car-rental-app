2f16eed1d3bb582acfa2c1f56d99742ce3c84468
KuhaiAnna <kuhai.anna@gmail.com>
2023-09-07 02:25:33 +0300

    Add store and persistor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { ThemeProvider } from 'styled-components';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
+import 'modern-normalize';
+import { GlobalStyle } from 'GlobalStyles';
 import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/car-rental-app">
+          <App />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
