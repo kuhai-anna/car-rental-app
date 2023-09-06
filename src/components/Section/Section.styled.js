de8ec9282bb80252c111ca9c06cb42915aaf7ee1
KuhaiAnna <kuhai.anna@gmail.com>
2023-09-07 02:43:34 +0300

    Create Section.styled.js

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,53 @@
+// import styled from 'styled-components';
+
+// export const SectionWrapper = styled.section`
+//   padding: ${props => props.theme.spacing(6)} ${props => props.theme.spacing(0)};
+// `;
+
+// export const Container = styled.div`
+//   margin: 0 auto;
+//   padding: ${props => props.theme.spacing(0)} ${props => props.theme.spacing(4)};
+//   width: ${props => props.theme.spacing(80)};
+// `;
+
+// export const MainTitle = styled.h1`
+//   margin-bottom: ${props => props.theme.spacing(5)};
+
+//   font-size: 30px;
+//   text-align: center;
+//   color: ${props => props.theme.colors.text};
+// `;
+
+// export const SectionTitle = styled.h2`
+//   margin-bottom: ${props => props.theme.spacing(4)};
+
+//   font-size: 24px;
+//   text-align: center;
+//   color: ${props => props.theme.colors.text};
+// `;
+
+import styled from 'styled-components';
+
+export const SectionWrapper = styled.section`
+  padding: 16px 0;
+`;
+
+export const Container = styled.div`
+  margin: 0 auto;
+  padding: 0 16px;
+  width: 320px;
+`;
+
+export const MainTitle = styled.h1`
+  margin-bottom: 20px;
+
+  font-size: 30px;
+  text-align: center;
+`;
+
+export const SectionTitle = styled.h2`
+  margin-bottom: 16px;
+
+  font-size: 24px;
+  text-align: center;
+`;
